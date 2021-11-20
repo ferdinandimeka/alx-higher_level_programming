fcdd6b7f5d12e5acabde6f9556885b2e131332fb
Imekaferdy <Imekaferdy20@gmail.com>
2021-11-20 00:54:32 -0800

    1. secure access to an element in a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        print('None')
+    if idx > len(my_list):
+        print('None')
+    indexof = my_list[idx]
+    return (indexof)

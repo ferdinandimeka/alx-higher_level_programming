d9da158f4b4104747e2a66efaf57a8978acd315b
Imekaferdy <Imekaferdy20@gmail.com>
2021-11-24 09:27:57 -0800

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
 import sys
 
 def main():
@@ -14,4 +13,5 @@ def main():
         for i in range(1, argc + 1):
             print("{}: {}".format(i, sys.argv[i]))
 
+if __name__ == "__main__":
 main()

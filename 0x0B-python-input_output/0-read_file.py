5f4b7123d499810da534a0fb6cdc045047a9a395
Imekaferdy <Imekaferdy20@gmail.com>
2021-12-22 08:41:09 -0800

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-""" 
-A function that reads a text file
-"""
+"""the read_file function """
+
 
 def read_file(filename=""):
-    with open(filename, 'r') as f:
-        fo = f.read()
-        print(fo)
-
+    """""function that reads a text file (UTF8) and prints it to stdout:"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end='')
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
- writes a string to a text file
-"""
+"""the write_file function """
+
 
 def write_file(filename="", text=""):
-    count = 0
-    with open("filename", "r") as fp:
-        for line in fp:
-            count += 1
-        return count
+    """function that writes a string to a text file
+     (UTF8) and returns the number of characters written"""
+    with open(filename, "w", encoding="UTF-8") as f:
+        return(f.write(text))

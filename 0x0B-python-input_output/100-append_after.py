c3ad482bf192929d4f9ce882eaecf3163080ab74
Imekaferdy <Imekaferdy20@gmail.com>
2021-12-23 11:51:13 -0800

    stop and search

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -20,4 +20,5 @@ def append_after(filename="", search_string="", new_string=""):
                 new_text += new_string
         f.seek(0)
         f.write(new_text)
-        print()
+
+    print()

245741b3bf25c51c15230c56198715a9bf55a04e
Imekaferdy <Imekaferdy20@gmail.com>
2022-02-28 09:08:24 -0800

    1. 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+//a script that prints 3 lines
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'Javascript is amazing';
+
+console.log(firstLine);
+console.log(secondLine);
+consol.log(thirdLine);

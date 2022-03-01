348bc6446f74f2e405c3261f0480d7f0e2c4f064
Imekaferdy <Imekaferdy20@gmail.com>
2022-03-01 04:27:45 -0800

    2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-import { argv } from 'process';
 
 //a script that prints a message depending of the number of arguments passed
-if (argv.length == 0) {
-  console.log('No argument');
-}
-else {
-  console.log('Argument found');
-};
+const args = process.argv.length - 2;
+
+args < 1 ? console.log('No argument') : args === 1 ? console.log('Argument found') : console.log('Arguments found');
+? console.log('No argument') : args === 1 ? console.log('Argument found') : console.log('Arguments found');  
 

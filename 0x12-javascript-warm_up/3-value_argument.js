b329e0e469833bae1067cb93d44ec51b61748507
Imekaferdy <Imekaferdy20@gmail.com>
2022-03-01 09:12:25 -0800

    3. Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-//a script that prints the first argument passed to it:
-const argv1 = process.argv[2];
-argv1 ? console.log(argv1) : console.log('No argument');
+//a script that prints the first argument passed to it.
+console.log(process.argv[2] || 'No argument');

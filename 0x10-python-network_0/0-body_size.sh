d59770600f560e7a50905a544ec544eadc74a32e
Imekaferdy <Imekaferdy20@gmail.com>
2022-03-21 04:09:08 -0700

    0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to that URL, and displays the size of the body of the response
-curl -so /dev/null "$1" -w '%{size_download}\n'
+curl -w '%{size_download}\n' -so /dev/null $1

5362be78eb01c02e950afbb66ac518a63dd450e7
Imekaferdy <Imekaferdy20@gmail.com>
2022-03-21 08:36:06 -0700

    0x11-python-network_1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""  Python script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+
+
+if __name__ == "__main__":
+    gr = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(gr.text)))
+    print('\t- content: {}'.format(gr.text))

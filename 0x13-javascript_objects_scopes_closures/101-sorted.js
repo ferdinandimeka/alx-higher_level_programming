b670ba7e19d58327a9c2640d546b8635cf02e191
imekaferdy <imekaferdy20@gmail.com>
2022-06-11 20:26:09 -0700

    update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,10 +4,10 @@ const dictInput = require('./101-data').dict;
 const outDict = {};
 
 for (const key in dictInput) {
-	if (dictInput[key] in outDict) {
-		outDict[dictInput[key]].push(key);
-	} else {
-		outDict[dictInput[key]] = [key];
-	}
+  if (dictInput[key] in outDict) {
+    outDict[dictInput[key]].push(key);
+  } else {
+    outDict[dictInput[key]] = [key];
+  }
 }
 console.log(outDict);

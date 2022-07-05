1c81bb7961f2720172b97fe18a60088dadecfa56
imekaferdy <imekaferdy20@gmail.com>
2022-07-05 14:33:31 -0700

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,4 +11,6 @@ class MyList(list):
         """
         def print_sorted(self):
             """print sorted list"""
-            print(sorted(self))
+            new_list = self[:]
+            new_list.sort()
+            print("{}".format(new_list))

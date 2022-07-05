8a69999a0274236be97e4805e3d810e0de3e2aa5
imekaferdy <imekaferdy20@gmail.com>
2022-07-05 14:17:23 -0700

    update

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,12 +7,13 @@ Class:
 
 
 class MyInt(int):
-        """ __eq__ and __ne__ inverted """
 
-        def __eq__(self, other):
-            """ Inverted to not equal """
-            return int.__ne__(self, other)
+    """ __eq__ and __ne__ inverted """
 
-        def __ne__(self, other):
-            """ Inverted to equal """
-            return int.__eq__(self, other)
+    def __eq__(self, other):
+        """ Inverted to not equal """
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """ Inverted to equal """
+        return int.__eq__(self, other)

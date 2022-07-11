b0aa74be8dcc99ce08c33fa4242df44d99e80e4f
imekaferdy <imekaferdy20@gmail.com>
2022-07-11 04:04:22 +0100

    update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 
 """empty class for rectangle"""
-
-<<<<<<< HEAD
-
-class Rectangle:
-    """had to put a comment here"""
-    pass
-=======
 class Rectangle:
     pass  # An empty block
->>>>>>> 7d845eae8e66c2e489fffce5a19a9f8c37228e1c

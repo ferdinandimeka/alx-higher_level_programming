0306bd99d2003b9d2789febde9453b3d68997384
ferdinandimeka <117474473+ferdinandimeka@users.noreply.github.com>
2023-05-02 01:03:11 +0100

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,14 @@ import requests
 
 
 if __name__ == "__main__":
-    gr = requests.get('https://alx-intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(gr.text)))
-    print('\t- content: {}'.format(gr.text))
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
+    
+    //gr = requests.get('https://alx-intranet.hbtn.io/status')
+    //print('Body response:')
+    //print('\t- type: {}'.format(type(gr.text)))
+    //print('\t- content: {}'.format(gr.text))
